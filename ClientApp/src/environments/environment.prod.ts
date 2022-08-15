f3bb19b9812a417df6662766b4b853216859cc4a
Lucas Nobrega <lucasignacionobrega@gmail.com>
2022-08-15 09:10:42 -0300

    Variaveis prod

--- ClientApp/src/environments/environment.prod.ts
+++ ClientApp/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyA0YZ9ZKVxsnbjYZphku4fuDpF1MI9e3kk",
+    authDomain: "biblioteca-gamificada.firebaseapp.com",
+    projectId: "biblioteca-gamificada",
+    storageBucket: "biblioteca-gamificada.appspot.com",
+    messagingSenderId: "152569540708",
+    appId: "1:152569540708:web:14217f798908e4e96e6f71",
+    measurementId: "G-RH0PZQ8XB4"
+  }
 };
