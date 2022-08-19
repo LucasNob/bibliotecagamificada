42b9d12673f6b654afbfd488b8b62cbc73ab678e
Matheus Lima de Almeida <matheuslima.tipi@gmail.com>
2022-08-19 17:11:05 -0300

    Logica Lista de Professores

--- ClientApp/src/app/components/professor/cadastro-professor/professor-lista/professor-lista.component.ts
+++ ClientApp/src/app/components/professor/cadastro-professor/professor-lista/professor-lista.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Professor } from 'src/app/models/entidades/Professor.model';
 
 @Component({
   selector: 'app-professor-lista',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfessorListaComponent implements OnInit {
 
+  @Input()
+  listaProfessor: Array<Professor> = [];
+
+  @Output()
+  excluirEmitter = new EventEmitter<string>();
+  
+  @Output()
+  editarEmitter = new EventEmitter<string>();
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+  obterLista() {
+    if (this.listaProfessor)
+      return this.listaProfessor;
+    return [];
+  }
+  Excluir(id: string) {
+    this.excluirEmitter.emit(id);
+  }
+  Editar(id: string) {
+    this.editarEmitter.emit(id);
   }
 
 }
