ed6712672e8de3d5ce939e28a2c7bd76d16d87b5
Matheus Lima de Almeida <matheuslima.tipi@gmail.com>
2022-10-17 15:21:42 -0300

    Botão de Acesso ao Ranking Escolar

---
+++ ClientApp/src/app/components/classificacao/classificacao-escolar-pagina/classificacao-escolar-pagina.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-classificacao-escolar-pagina',
+  templateUrl: './classificacao-escolar-pagina.component.html',
+  styleUrls: ['./classificacao-escolar-pagina.component.css']
+})
+export class ClassificacaoEscolarPaginaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
